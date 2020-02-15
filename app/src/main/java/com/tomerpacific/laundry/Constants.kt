c58e7131fd40585f37a537f7572541517dfe26f0
unknown <tomerpacific@gmail.com>
2020-02-15 09:24:55 +0200

    LaundrySymbols adding logic to handle in app updates

--- app/src/main/java/com/tomerpacific/laundry/Constants.kt
+++ app/src/main/java/com/tomerpacific/laundry/Constants.kt
@@ -11,5 +11,7 @@ const val SYMBOL_NAME_KEY = "symbol_name"
 const val SYMBOL_IMAGE_KEY = "symbol_image"
 const val SYMBOL_DESCRIPTION_KEY = "symbol_description"
 const val DRAWABLE_ASSET_PREFIX_PATH = "res/"
+const val UPDATE_REQUEST_CODE = 100
+
 
 
--- app/src/main/java/com/tomerpacific/laundry/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/laundry/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.tomerpacific.laundry
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.tomerpacific.laundry.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG : String = MainActivity::class.java.simpleName
+    var appUpdateManager : AppUpdateManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupDataBinding()
+
+        Utilities.setFont(this, BANGERS_FONT, R.id.textView)
+        val versionTextView : TextView = findViewById(R.id.app_version)
+        versionTextView.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
+        setListenersForButtons()
+        checkForUpdate()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { result: AppUpdateInfo? ->
+            if (result?.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager?.startUpdateFlowForResult(result,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    UPDATE_REQUEST_CODE)
+            }
+        }
+
+    }
+
+    private fun setupDataBinding() {
+
         val binding : ActivityMainBinding = DataBindingUtil.setContentView(
             this, R.layout.activity_main)
         val washingDrawable = resources.getDrawable(R.drawable.washable)
@@ -27,14 +61,6 @@ class MainActivity : AppCompatActivity() {
 
         val customButtons : List<Symbol> = listOf(washingSymbol, bleachingSymbol,dryingSymbol, ironingSymbol)
         binding.symbols = SymbolData(customButtons)
-
-        Utilities.setFont(this, BANGERS_FONT, R.id.textView)
-
-        val versionTextView : TextView = findViewById(R.id.app_version)
-        versionTextView.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
-        setListenersForButtons()
-
-
     }
 
     private fun setListenersForButtons() {
@@ -66,6 +92,45 @@ class MainActivity : AppCompatActivity() {
 
         if (activityToOpen != null) {
             startActivity(activityToOpen)
+        }
+    }
+
+
+    private fun checkForUpdate() {
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+
+        val appUpdateInfoTask = appUpdateManager?.appUpdateInfo
+
+        appUpdateInfoTask?.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                try {
+                    appUpdateManager?.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        UPDATE_REQUEST_CODE)
+                } catch(e: Exception) {
+                    e.printStackTrace()
+                }
+            } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED){
+                //Already downloaded update, do nothing
+                Log.d(TAG, "checkForUpdate update downloaded")
+            } else  if (appUpdateInfo.installStatus() == InstallStatus.FAILED ||
+                        appUpdateInfo.installStatus() == InstallStatus.UNKNOWN) {
+                Log.e(TAG, "checkForUpdate checkForUpdate updateAvailability FAILED")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == UPDATE_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+                Log.e(TAG, "onActivityResult: app download failed")
+            }
         }
     }
 
