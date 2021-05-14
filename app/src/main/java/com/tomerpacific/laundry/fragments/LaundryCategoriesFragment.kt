28a851c8030487d5c43965bb66de86fa60c79106
unknown <tomerpacific@gmail.com>
2021-05-14 13:28:33 +0300

    LaundrySymbols adding fragments for specific laundy category and for all laundry categories

---
+++ app/src/main/java/com/tomerpacific/laundry/fragments/LaundryCategoriesFragment.kt
@@ -0,0 +1,45 @@
+package com.tomerpacific.laundry.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.tomerpacific.laundry.BANGERS_FONT
+import com.tomerpacific.laundry.BuildConfig
+import com.tomerpacific.laundry.R
+import com.tomerpacific.laundry.Utilities
+
+class LaundryCategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_laundry_categories, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        setFontAndVersion()
+    }
+
+    private fun setFontAndVersion() {
+        Utilities.setFont(
+            view,
+            requireActivity(),
+            BANGERS_FONT,
+            R.id.textView
+        )
+
+        view?.findViewById<TextView>(R.id.app_version).apply {
+            this?.text = getString(
+                R.string.app_version,
+                BuildConfig.VERSION_NAME
+            )
+        }
+    }
+}
--- app/src/main/java/com/tomerpacific/laundry/fragments/LaundryCategoryFragment.kt
+++ app/src/main/java/com/tomerpacific/laundry/fragments/LaundryCategoryFragment.kt
@@ -1,4 +1,34 @@
 package com.tomerpacific.laundry.fragments
 
-class LaundryCategoryFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.tomerpacific.laundry.R
+
+class LaundryCategoryFragment : Fragment() {
+
+    companion object {
+        fun newInstance(laundryCategory: String) : LaundryCategoryFragment {
+            val fragment = LaundryCategoryFragment()
+            val args = Bundle()
+            args.putString("laundry_category", laundryCategory)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_laundry_category, container, false)
+        val laundryCategoryTextView : TextView = view.findViewById<TextView>(R.id.laundry_category_textview)
+        laundryCategoryTextView.text = this.arguments?.getString("laundry_category")
+        return view;
+    }
+
 }
