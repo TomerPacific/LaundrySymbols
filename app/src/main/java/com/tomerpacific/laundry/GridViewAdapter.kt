72d1003d43f44b0f096c8dc4b17af7321644f2ac
unknown <tomerpacific@gmail.com>
2021-05-15 15:08:24 +0300

    LaundrySymbols converting layout to use grid view and creating adapte to handle UI

--- app/src/main/java/com/tomerpacific/laundry/GridViewAdapter.kt
+++ app/src/main/java/com/tomerpacific/laundry/GridViewAdapter.kt
@@ -1,4 +1,43 @@
 package com.tomerpacific.laundry
 
-class GridViewAdapter {
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+class GridViewAdapter(var data: List<String>) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflator: LayoutInflater = parent?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val gridItemView = inflator.inflate(R.layout.grid_view_item, null)
+        val imageView: ImageView = gridItemView.findViewById(R.id.image_button)
+        val identifier: Int? = parent.context?.resources?.getIdentifier(
+            data[position],
+            "drawable",
+            parent.context?.packageName)
+
+        imageView.apply {
+            scaleType = ImageView.ScaleType.CENTER_CROP
+            setImageResource(identifier!!)
+            tag = data[position]
+        }
+
+        return gridItemView
+    }
+
+    override fun getItem(p0: Int): Any {
+        return data[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return data[p0].toLong()
+    }
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
 }
