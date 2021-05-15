0865137a72c49aca0a7a6508655240c17c85d5e4
unknown <tomerpacific@gmail.com>
2021-05-15 15:07:26 +0300

    LaundrySymbols logic to generate symbols per category

---
+++ app/src/main/java/com/tomerpacific/laundry/GridViewAdapter.kt
@@ -0,0 +1,4 @@
+package com.tomerpacific.laundry
+
+class GridViewAdapter {
+}
--- app/src/main/java/com/tomerpacific/laundry/fragments/LaundryCategoryFragment.kt
+++ app/src/main/java/com/tomerpacific/laundry/fragments/LaundryCategoryFragment.kt
@@ -4,19 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
-import com.tomerpacific.laundry.BANGERS_FONT
-import com.tomerpacific.laundry.R
-import com.tomerpacific.laundry.Utilities
+import com.tomerpacific.laundry.*
 
 class LaundryCategoryFragment : Fragment() {
+
+    private lateinit var gridViewAdapter: GridViewAdapter
 
     companion object {
         fun newInstance(laundryCategory: String) : LaundryCategoryFragment {
             val fragment = LaundryCategoryFragment()
             val args = Bundle()
-            args.putString("laundry_category", laundryCategory)
+            args.putString(LAUNDRY_CATEGORY_KEY, laundryCategory)
             fragment.arguments = args
             return fragment
         }
@@ -29,7 +29,8 @@ class LaundryCategoryFragment : Fragment() {
     ): View? {
         val view : View = inflater.inflate(R.layout.fragment_laundry_category, container, false)
         val laundryCategoryTextView : TextView = view.findViewById<TextView>(R.id.laundry_category_textview)
-        laundryCategoryTextView.text = this.arguments?.getString("laundry_category")
+        val laundryCategory : String? = this.arguments?.getString(LAUNDRY_CATEGORY_KEY)
+        laundryCategoryTextView.text = laundryCategory
 
         Utilities.setFont(
             view,
@@ -37,7 +38,29 @@ class LaundryCategoryFragment : Fragment() {
             BANGERS_FONT,
             R.id.laundry_category_textview
         )
+
+        val gridLayout: GridView = view.findViewById(R.id.grid_layout)
+        setupLayoutForLaundryCategory(laundryCategory, gridLayout)
         return view;
     }
+
+    private fun setupLayoutForLaundryCategory(laundryCategory: String?, gridLayout: GridView) {
+        val buttonsToGenerate: List<String> = when(laundryCategory) {
+            "Washing" -> listOf("wash_do_not",
+                "washable",
+                "wash_30_degrees",
+                "wash_40_degrees_warm",
+                "wash_60_degrees_hot",
+                "wash_30_degrees_delicate",
+                "wash_30_double_line",
+                "wash_by_hand"
+            )
+            else -> listOf()
+        }
+
+        gridViewAdapter = GridViewAdapter(buttonsToGenerate)
+        gridLayout.adapter = gridViewAdapter
+    }
+
 
 }
