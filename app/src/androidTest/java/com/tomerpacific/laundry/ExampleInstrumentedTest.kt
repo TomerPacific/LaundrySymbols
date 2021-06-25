29c56d3fc5308c28cf80b9b9445bf041a31d0f00
unknown <tomerpacific@gmail.com>
2021-06-25 21:28:29 +0300

    LaundrySymbols adding unit test to check if click on washing button makes application go to washing fragment

--- app/src/androidTest/java/com/tomerpacific/laundry/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tomerpacific/laundry/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.tomerpacific.laundry
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.tomerpacific.laundry.activity.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.tomerpacific.laundry", appContext.packageName)
+    }
+
+    @Test
+    fun go_to_washing_symbols() {
+        onView(withId(R.id.washing_imageview)).perform(click())
+        onView(withId(R.id.laundry_category_textview)).check(matches(withText(R.string.washing)))
     }
 }
