a89c1ff27c324d266bf0b2100227c91c3a3c801e
unknown <tomerpacific@gmail.com>
2021-11-12 13:53:11 +0200

    LaundrySymbols updating LaundrySymbol model constructor

--- app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
@@ -1,7 +1,8 @@
 package com.tomerpacific.laundry.model
 
-data class LaundrySymbol {
-    var name: String
-    var description: String
-    var image: String
+data class LaundrySymbol(
+    var name: String,
+    var description: String,
+    var image: String) {
+
 }
