17b8bc089c2134b82e469bd49e897c69249f4835
unknown <tomerpacific@gmail.com>
2021-11-14 21:07:01 +0200

    LaundrySymbols changing type of list to LaundryCategory

--- app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.tomerpacific.laundry.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tomerpacific.laundry.model.LaundryCategory
 
 class MainViewModel: ViewModel() {
-    var laundryCategories = MutableLiveData<ArrayList<String>>()
+    var laundryCategories = MutableLiveData<ArrayList<LaundryCategory>>()
+
+
 
 }
