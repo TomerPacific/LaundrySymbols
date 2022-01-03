6a0474d242b696dcd4eafe0388b836bbadcff295
unknown <tomerpacific@gmail.com>
2022-01-03 21:33:58 +0200

    LaundrySymbols removing unused import

--- app/src/androidTest/java/com/tomerpacific/laundry/LaundryCategoryInstrumentedTest.kt
+++ app/src/androidTest/java/com/tomerpacific/laundry/LaundryCategoryInstrumentedTest.kt
@@ -2,7 +2,6 @@ package com.tomerpacific.laundry
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
