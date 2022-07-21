6b8f7dac70d44e254c55fd7db31e5280caf70851
unknown <tomerpacific@gmail.com>
2022-07-21 21:30:38 +0300

    LaundrySymbols removing unused method in utilities

--- app/src/main/java/com/tomerpacific/laundry/Utilities.kt
+++ app/src/main/java/com/tomerpacific/laundry/Utilities.kt
@@ -19,26 +19,6 @@ class Utilities {
             TooltipCompat.setTooltipText(imageButton, symbolDescription)
         }
 
-        fun getLaundryCategoryDrawableId(laundryCategoryName: String) : Int {
-            return when (laundryCategoryName) {
-                LAUNDRY_CATEGORY_WASHING -> R.id.washing_imageview
-                LAUNDRY_CATEGORY_BLEACHING -> R.id.bleaching_imageview
-                LAUNDRY_CATEGORY_DRYING -> R.id.drying_imageview
-                LAUNDRY_CATEGORY_IRONING -> R.id.ironing_imageview
-                else -> -1
-            }
-        }
-
-        fun getLaundryCategoryTextviewId(laundryCategoryName: String) : Int {
-            return when (laundryCategoryName) {
-                LAUNDRY_CATEGORY_WASHING -> R.id.washing_textview
-                LAUNDRY_CATEGORY_BLEACHING -> R.id.bleaching_textview
-                LAUNDRY_CATEGORY_DRYING -> R.id.drying_textview
-                LAUNDRY_CATEGORY_IRONING -> R.id.ironing_textview
-                else -> -1
-            }
-        }
-
     }
 
 }
