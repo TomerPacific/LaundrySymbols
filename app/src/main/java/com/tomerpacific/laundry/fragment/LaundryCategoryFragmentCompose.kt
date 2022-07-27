3e74d5f7ed5ccbfd53c761701c55e45f6557a58d
unknown <tomerpacific@gmail.com>
2022-07-27 21:30:44 +0300

    LaundrySymbols using tooltip to display laundry symbol name

--- app/src/main/java/com/tomerpacific/laundry/fragment/LaundryCategoryFragmentCompose.kt
+++ app/src/main/java/com/tomerpacific/laundry/fragment/LaundryCategoryFragmentCompose.kt
@@ -1,20 +1,26 @@
 package com.tomerpacific.laundry.fragment
 
+import Tooltip
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -54,27 +60,49 @@ class LaundryCategoryFragmentCompose: Fragment() {
                         fontFamily = Bangers,
                         fontSize = 30.sp
                     )
-                    LazyVerticalGrid(cells = GridCells.Fixed(3), modifier = Modifier.fillMaxSize(),
+                    LazyVerticalGrid(
+                        cells = GridCells.Fixed(3), modifier = Modifier.fillMaxSize(),
                         contentPadding = PaddingValues(12.dp),
                         verticalArrangement = Arrangement.spacedBy(10.dp),
-                        horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                        horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
                         items(model.getItemsForLaundryCategory(laundryCategoryName)) { laundrySymbol ->
-                            Image(painterResource
-                                (laundrySymbol.drawableId),
-                                laundrySymbol.description,
-                            modifier = Modifier
-                                .width(100.dp)
-                                .height(100.dp)
-                                .border(BorderStroke(2.dp, Color.Black))
-                                .clickable(enabled = true, onClick = {
-                                    val fragment : LaundrySymbolFragmentCompose = LaundrySymbolFragmentCompose.newInstance(laundrySymbol)
-                                    model.handleClickOnLaundrySymbol(requireActivity(), fragment)
-                                })
-                            )
+                            Box {
+                                val showTooltip = remember { mutableStateOf(false) }
+
+                                Box(
+                                    modifier = Modifier
+                                        .combinedClickable(
+                                            interactionSource = remember { MutableInteractionSource() },
+                                            indication = rememberRipple(),
+                                            onClickLabel = "Laundry Symbol Name",
+                                            role = Role.Button,
+                                            onClick = {
+                                                val fragment: LaundrySymbolFragmentCompose =
+                                                LaundrySymbolFragmentCompose.newInstance(laundrySymbol)
+                                                model.handleClickOnLaundrySymbol(requireActivity(), fragment)
+                                            },
+                                            onLongClick = { showTooltip.value = true },
+                                        ),
+                                ) {
+                                    Image(painterResource
+                                        (laundrySymbol.drawableId),
+                                        laundrySymbol.description,
+                                        modifier = Modifier
+                                            .width(100.dp)
+                                            .height(100.dp)
+                                            .border(BorderStroke(2.dp, Color.Black))
+                                    )
+                                }
+
+                                Tooltip(showTooltip) {
+                                    Text(laundrySymbol.name)
+                                }
+                            }
                         }
                     }
-                }
 
+                }
             }
         }
     }
