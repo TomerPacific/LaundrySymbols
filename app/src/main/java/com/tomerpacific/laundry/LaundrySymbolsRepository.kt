55f4dc439ec1de6dbbd616aa6d5badad9db29685
unknown <tomerpacific@gmail.com>
2023-11-07 21:38:04 +0200

    LaundrySymbols replacing with string values

--- app/src/main/java/com/tomerpacific/laundry/LaundrySymbolsRepository.kt
+++ app/src/main/java/com/tomerpacific/laundry/LaundrySymbolsRepository.kt
@@ -17,39 +17,39 @@ class LaundrySymbolsRepository {
         )
     }
 
-    fun createBleachingSymbols() : List<LaundrySymbol> {
+    fun createBleachingSymbols(context: Context) : List<LaundrySymbol> {
         return listOf(
-            LaundrySymbol("Do Not Bleach", "Do Not Bleach", R.drawable.bleach_do_not),
-            LaundrySymbol("Bleaching Allowed", "Bleaching Allowed", R.drawable.bleach_allow),
-            LaundrySymbol("Bleach Non Chlorine", "Bleach Non Chlorine", R.drawable.bleach_non_chlorine)
+            LaundrySymbol(context.getString(R.string.do_not_bleach_symbol), context.getString(R.string.do_not_bleach_symbol), R.drawable.bleach_do_not),
+            LaundrySymbol(context.getString(R.string.bleaching_allowed_symbol), context.getString(R.string.bleaching_allowed_symbol), R.drawable.bleach_allow),
+            LaundrySymbol(context.getString(R.string.bleaching_non_chlorine_symbol), context.getString(R.string.bleaching_non_chlorine_symbol), R.drawable.bleach_non_chlorine)
         )
     }
 
-    fun createDryingSymbols(): List<LaundrySymbol> {
+    fun createDryingSymbols(context: Context): List<LaundrySymbol> {
         return listOf(
-            LaundrySymbol("Do Not Dry Clean", "Do Not Dry Clean", R.drawable.dry_cleaning_do_not),
-            LaundrySymbol("Dry Cleaning Allowed", "Dry Cleaning Allowed", R.drawable.dry_cleaning_allow),
-            LaundrySymbol("Dry Cleaning Low Heat", "Dry Cleaning Low Heat", R.drawable.dry_cleaning_low_heat),
-            LaundrySymbol("Dry Cleaning No Steam", "Dry Cleaning No Steam", R.drawable.dry_cleaning_no_steam),
-            LaundrySymbol("Dry Cleaning Any Solvent", "Dry Cleaning Any Solvent", R.drawable.dry_cleaning_a),
-            LaundrySymbol("Dry Cleaning Only", "Dry Cleaning Only", R.drawable.dry_cleaning_p),
-            LaundrySymbol("Dry Cleaning Petroleum Solvent Only", "Dry Cleaning Petroleum Solvent Only", R.drawable.dry_cleaning_f),
-            LaundrySymbol("Tumble Dry Low Heat", "Tumble Dry Low Heat", R.drawable.tumble_dry_low),
-            LaundrySymbol("Tumble Dry High Heat", "Tumble Dry High Heat", R.drawable.tumble_dry_high),
-            LaundrySymbol("Dry Flat", "Dry Flat", R.drawable.natural_drying_one_line),
-            LaundrySymbol("Hang To Dry", "Hang To Dry", R.drawable.natural_drying_hang_to_dry),
-            LaundrySymbol("Do Not Tumble Dry", "Do Not Tumble Dry", R.drawable.dryer_do_not_tumble_dry)
+            LaundrySymbol(context.getString(R.string.do_not_dry_clean_symbol), context.getString(R.string.do_not_dry_clean_symbol), R.drawable.dry_cleaning_do_not),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_allowed_symbol), context.getString(R.string.dry_cleaning_allowed_symbol), R.drawable.dry_cleaning_allow),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_low_heat_symbol), context.getString(R.string.dry_cleaning_low_heat_symbol), R.drawable.dry_cleaning_low_heat),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_no_steam_symbol), context.getString(R.string.dry_cleaning_no_steam_symbol), R.drawable.dry_cleaning_no_steam),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_any_solvent_symbol), context.getString(R.string.dry_cleaning_any_solvent_symbol), R.drawable.dry_cleaning_a),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_only_symbol), context.getString(R.string.dry_cleaning_only_symbol), R.drawable.dry_cleaning_p),
+            LaundrySymbol(context.getString(R.string.dry_cleaning_petroleum_solvent_only_symbol), context.getString(R.string.dry_cleaning_petroleum_solvent_only_symbol), R.drawable.dry_cleaning_f),
+            LaundrySymbol(context.getString(R.string.tumble_dry_low_heat_symbol), context.getString(R.string.tumble_dry_low_heat_symbol), R.drawable.tumble_dry_low),
+            LaundrySymbol(context.getString(R.string.tumble_dry_high_heat_symbol), context.getString(R.string.tumble_dry_high_heat_symbol), R.drawable.tumble_dry_high),
+            LaundrySymbol(context.getString(R.string.dry_flat_symbol), context.getString(R.string.dry_flat_symbol), R.drawable.natural_drying_one_line),
+            LaundrySymbol(context.getString(R.string.hang_to_dry_symbol), context.getString(R.string.hang_to_dry_symbol), R.drawable.natural_drying_hang_to_dry),
+            LaundrySymbol(context.getString(R.string.do_not_tumble_dry_symbol), context.getString(R.string.do_not_tumble_dry_symbol), R.drawable.dryer_do_not_tumble_dry)
         )
     }
 
-    fun createIroningSymbols(): List<LaundrySymbol> {
+    fun createIroningSymbols(context: Context): List<LaundrySymbol> {
         return listOf(
-            LaundrySymbol("Do Not Iron", "Do Not Iron", R.drawable.iron_do_not),
-            LaundrySymbol("Ironing Allowed", "Ironing Allowed", R.drawable.iron_allowed),
-            LaundrySymbol("Iron Low (Max 110\u00B0 Celsius)", "Iron Low (Max 110\u00B0 Celsius)", R.drawable.iron_low_setting),
-            LaundrySymbol("Iron Medium (Max 150\u00B0 Celsius)", "Iron Medium (Max 150\u00B0 Celsius)", R.drawable.iron_medium_setting),
-            LaundrySymbol( "Iron High (Max 200\u00B0 Celsius)",  "Iron High (Max 200\u00B0 Celsius)", R.drawable.iron_high_setting),
-            LaundrySymbol( "Steaming Not Allowed",  "Steaming Not Allowed", R.drawable.iron_steam_not_allowed)
+            LaundrySymbol(context.getString(R.string.do_not_iron_symbol), context.getString(R.string.do_not_iron_symbol), R.drawable.iron_do_not),
+            LaundrySymbol(context.getString(R.string.ironing_allowed_symbol), context.getString(R.string.ironing_allowed_symbol), R.drawable.iron_allowed),
+            LaundrySymbol(context.getString(R.string.iron_low_symbol), context.getString(R.string.iron_low_symbol), R.drawable.iron_low_setting),
+            LaundrySymbol(context.getString(R.string.iron_medium_symbol), context.getString(R.string.iron_medium_symbol), R.drawable.iron_medium_setting),
+            LaundrySymbol( context.getString(R.string.tumble_dry_high_heat_symbol),  context.getString(R.string.tumble_dry_high_heat_symbol), R.drawable.iron_high_setting),
+            LaundrySymbol( context.getString(R.string.steaming_not_allowed_symbol),  context.getString(R.string.steaming_not_allowed_symbol), R.drawable.iron_steam_not_allowed)
         )
     }
 }
