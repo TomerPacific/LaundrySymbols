19a162ac3d0a8c8458f02458f1f39b0c6df4c4fc
unknown <tomerpacific@gmail.com>
2024-08-28 21:00:36 +0300

    LaundrySymbols adding enum for drawer categories and method to handle state in drawer

--- app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.tomerpacific.laundry.viewmodel
 
 import android.content.Context
+import androidx.compose.runtime.mutableStateOf
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import com.tomerpacific.laundry.*
@@ -9,9 +10,16 @@ import com.tomerpacific.laundry.fragment.LaundryCategoryFragment
 import com.tomerpacific.laundry.fragment.LaundrySymbolFragment
 import com.tomerpacific.laundry.model.LaundrySymbol
 
+
+enum class HowToDoLaundryDrawerItems {
+    SEPARATING_LAUNDRY,
+}
 class MainViewModel: ViewModel() {
 
     private val laundrySymbolsRepository = LaundrySymbolsRepository()
+
+    private val _selectedDrawerItem = mutableStateOf(HowToDoLaundryDrawerItems.SEPARATING_LAUNDRY)
+    val selectedDrawerItem = _selectedDrawerItem
 
     fun handleClickOnLaundryCategory(activity: FragmentActivity, fragment: LaundryCategoryFragment) {
         activity.supportFragmentManager.beginTransaction().
@@ -40,6 +48,12 @@ class MainViewModel: ViewModel() {
         activity.supportFragmentManager.beginTransaction().
         replace(R.id.fragment_container_view, HowToDoLaundryFragment()).
         addToBackStack(null).commit()
+    }
+
+    fun handleClickOnHowToDoLaundryCategories(categoryTitle: String) {
+        when (categoryTitle) {
+            HowToDoLaundryDrawerItems.SEPARATING_LAUNDRY.name -> _selectedDrawerItem.value = HowToDoLaundryDrawerItems.SEPARATING_LAUNDRY
+        }
     }
 
 }
