907c93d199116b801bf99439e55ef9f4d4c66cee
unknown <tomerpacific@gmail.com>
2024-08-28 09:04:13 +0300

    LaundrySymbols removing import

--- app/src/main/java/com/tomerpacific/laundry/fragment/LaundryCategoryFragment.kt
+++ app/src/main/java/com/tomerpacific/laundry/fragment/LaundryCategoryFragment.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipDefaults
