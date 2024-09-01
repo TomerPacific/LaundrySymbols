10d4f693d88b4a539d77aed983e5db77a86b2f29
unknown <tomerpacific@gmail.com>
2024-09-01 21:55:35 +0300

    LaundrySymbols adding method to convert enum to string

--- app/src/main/java/com/tomerpacific/laundry/model/HowToDoLaundryCategory.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/HowToDoLaundryCategory.kt
@@ -3,8 +3,18 @@ package com.tomerpacific.laundry.model
 enum class HowToDoLaundryCategories {
     SEPARATING_LAUNDRY,
     TREATING_STAINS,
-    LOAD_DETERGENT,
+    LOAD_DETERGENT;
+    companion object {
+        fun convertHowToDoLaundryCategory(category: HowToDoLaundryCategories): String {
+            return when(category) {
+                HowToDoLaundryCategories.SEPARATING_LAUNDRY -> "Separating Laundry"
+                HowToDoLaundryCategories.TREATING_STAINS -> "Treating Stains"
+                HowToDoLaundryCategories.LOAD_DETERGENT -> "Load Detergent"
+            }
+        }
+    }
 }
+
 data class HowToDoLaundryCategory(
     val name: HowToDoLaundryCategories,
     val descriptionId: Int,
