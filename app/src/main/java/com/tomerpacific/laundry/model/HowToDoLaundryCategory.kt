33b03c75dac8497d83c78b9e2582b9a4980abe8a
unknown <tomerpacific@gmail.com>
2024-10-18 19:16:58 +0300

    LaundrySymbols refactoring model to use resource ids for category names

--- app/src/main/java/com/tomerpacific/laundry/model/HowToDoLaundryCategory.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/HowToDoLaundryCategory.kt
@@ -1,26 +1,7 @@
 package com.tomerpacific.laundry.model
 
-enum class HowToDoLaundryCategories {
-    SEPARATING_LAUNDRY,
-    TREATING_STAINS,
-    LOAD_DETERGENT,
-    USING_BLEACH,
-    ADDING_FABRIC_SOFTENER,
-    WASH_CYCLE,
-    WASH_TEMPERATURE;
-    companion object {
-        fun convertToString(category: HowToDoLaundryCategories): String {
-            return category.name.split("_").joinToString(" ") { word ->
-                word.lowercase().replaceFirstChar(
-                    Char::titlecaseChar
-                )
-            }
-        }
-    }
-}
-
 data class HowToDoLaundryCategory(
-    val name: HowToDoLaundryCategories,
+    val name: Int,
     val descriptionId: Int,
     val drawableId: Int,
     val contentDescriptionId: Int,
