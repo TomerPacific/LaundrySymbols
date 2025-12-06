6ab567b4e8d3db4ca9558a583d8e49809fb95f50
Tomer Ben-Rachel <tomerpacific@gmail.com>
2025-12-06 20:13:09 +0200

    LaundrySymbols adding test class for back button

--- app/src/androidTest/java/com/tomerpacific/laundry/MainActivityTest.kt
+++ app/src/androidTest/java/com/tomerpacific/laundry/MainActivityTest.kt
@@ -1,4 +1,3 @@
-
 package com.tomerpacific.laundry
 
 import androidx.compose.ui.test.assertIsDisplayed
@@ -6,9 +5,10 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.NoActivityResumedException
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tomerpacific.laundry.activity.MainActivity
-import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,27 +21,22 @@ class MainActivityTest {
 
     @Test
     fun testBackNavigation_popsFragment() {
-        // Navigate to a fragment
         composeTestRule.onNodeWithTag("washing category").performClick()
         composeTestRule.onNodeWithTag("Do Not Wash").assertIsDisplayed()
 
-        // Press back
         Espresso.pressBack()
 
-        // Assert we are back to the main screen
         composeTestRule.onNodeWithTag("washing category").assertIsDisplayed()
         composeTestRule.onNodeWithTag("Do Not Wash").assertDoesNotExist()
     }
 
     @Test
     fun testBackNavigation_finishesActivity() {
-        // Assert activity is not finished
-        assertTrue(!composeTestRule.activity.isFinishing)
-
-        // Press back
-        Espresso.pressBack()
-
-        // Assert activity is finishing
-        assertTrue(composeTestRule.activity.isFinishing)
+        try {
+            Espresso.pressBack()
+            fail("Expected NoActivityResumedException to be thrown.")
+        } catch (e: NoActivityResumedException) {
+            // Expected exception: test passes.
+        }
     }
 }
