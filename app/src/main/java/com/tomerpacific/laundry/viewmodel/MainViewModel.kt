5dcd8cd9830853a2ca3a515c9b277e7ca88d0d8f
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-02-06 11:39:02 +0200

    LaundrySymbols removing click handlers

--- app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
@@ -3,12 +3,9 @@ package com.tomerpacific.laundry.viewmodel
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.UriHandler
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
-import com.tomerpacific.laundry.*
-import com.tomerpacific.laundry.fragment.HowToDoLaundryFragment
-import com.tomerpacific.laundry.fragment.LaundryCategoryFragment
-import com.tomerpacific.laundry.fragment.LaundrySymbolFragment
+import com.tomerpacific.laundry.R
+import com.tomerpacific.laundry.LaundrySymbolsRepository
 import com.tomerpacific.laundry.model.HowToDoLaundryCategory
 import com.tomerpacific.laundry.model.LaundrySymbol
 
@@ -28,18 +25,6 @@ class MainViewModel: ViewModel() {
         "https://play.google.com/store/apps/developer?id=tomerpacific"
     )
 
-    fun handleClickOnLaundryCategory(activity: FragmentActivity, fragment: LaundryCategoryFragment) {
-        activity.supportFragmentManager.beginTransaction().
-        replace(R.id.fragment_container_view, fragment).
-        addToBackStack(null).commit()
-    }
-
-    fun handleClickOnLaundrySymbol(activity: FragmentActivity, fragment: LaundrySymbolFragment) {
-        activity.supportFragmentManager.beginTransaction().
-        replace(R.id.fragment_container_view, fragment).
-        addToBackStack(null).commit()
-    }
-
     fun getItemsForLaundryCategory(laundryCategory: String, context: Context) : List<LaundrySymbol> {
 
         return when (laundryCategory) {
@@ -53,12 +38,6 @@ class MainViewModel: ViewModel() {
 
     fun getHowToDoLaundryCategories(): List<HowToDoLaundryCategory> {
         return howToDoLaundryCategories
-    }
-
-    fun handleClickOnLearnHowToDoLaundry(activity: FragmentActivity) {
-        activity.supportFragmentManager.beginTransaction().
-        replace(R.id.fragment_container_view, HowToDoLaundryFragment()).
-        addToBackStack(null).commit()
     }
 
     fun handleClickOnHowToDoLaundryCategories(howToDoLaundryCategory: HowToDoLaundryCategory) {
