7330009345e12ba96f8b7014a627a077a9f27589
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-02-06 16:02:17 +0200

    LaundrySymbols wrapping composable with scaffold

--- app/src/main/java/com/tomerpacific/laundry/ui/screens/LaundrySymbolScreen.kt
+++ app/src/main/java/com/tomerpacific/laundry/ui/screens/LaundrySymbolScreen.kt
@@ -4,9 +4,13 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,27 +22,31 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun LaundrySymbolScreen(symbolName: String, @DrawableRes symbolDrawableId: Int) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            symbolName,
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center
-        )
-        Image(
-            painter = painterResource(id = symbolDrawableId),
-            contentDescription = symbolName,
-            modifier = Modifier
-                .width(200.dp)
-                .height(200.dp)
-        )
-        Text(
-            symbolName,
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center
-        )
+    Scaffold(
+        contentWindowInsets = WindowInsets.safeContent
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding).fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                symbolName,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center
+            )
+            Image(
+                painter = painterResource(id = symbolDrawableId),
+                contentDescription = symbolName,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+            )
+            Text(
+                symbolName,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
