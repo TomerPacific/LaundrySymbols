070822e0053c297117d6fa20a4e629e1147aaf65
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-02-13 16:44:45 +0200

    LaundrySymbols removing unused method

--- app/src/main/java/com/tomerpacific/laundry/model/TemperatureUnit.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/TemperatureUnit.kt
@@ -3,8 +3,4 @@ package com.tomerpacific.laundry.model
 enum class TemperatureUnit {
     CELSIUS,
     FAHRENHEIT;
-
-    fun convertFromCelsiusToFahrenheit(celsius: Int): Int {
-        return (celsius * 9/5) + 32
-    }
 }
