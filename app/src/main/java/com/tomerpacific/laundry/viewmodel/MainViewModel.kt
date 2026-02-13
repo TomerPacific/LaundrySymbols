25d58ad4cae18a691b6bb62f2dd12e122b130597
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-02-13 16:48:33 +0200

    LaundrySymbols refactoring to expose only state

--- app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/laundry/viewmodel/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.tomerpacific.laundry.viewmodel
 
 
 import android.app.Application
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.UriHandler
 import androidx.lifecycle.AndroidViewModel
@@ -15,14 +16,14 @@ class MainViewModel(application: Application): AndroidViewModel(application ) {
     private val laundrySymbolsRepository = LaundrySymbolsRepository()
 
     private val _temperatureUnit = mutableStateOf(TemperatureUnit.CELSIUS)
-    val temperatureUnit = _temperatureUnit
+    val temperatureUnit: State<TemperatureUnit> = _temperatureUnit
 
     private val howToDoLaundryCategories = laundrySymbolsRepository.createHowToDoLaundryCategories()
 
     private val laundryCategoryItems = laundrySymbolsRepository.createLaundryCategoryItems(application)
 
     private val _selectedDrawerItem = mutableStateOf(howToDoLaundryCategories[0])
-    val selectedDrawerItem = _selectedDrawerItem
+    val selectedDrawerItem: State<HowToDoLaundryCategory> = _selectedDrawerItem
 
     private val websiteUrls = listOf(
         "https://tomerpacific.github.io/Portfolio/",
