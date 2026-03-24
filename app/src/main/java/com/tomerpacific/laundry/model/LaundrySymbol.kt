6dd08e9191fcea6818f82a47d497762dd9054409
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-24 20:44:23 +0200

    LaundrySymbols creating symbolId

--- app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
@@ -2,8 +2,11 @@ package com.tomerpacific.laundry.model
 
 import androidx.annotation.DrawableRes
 
+@JvmInline
+value class SymbolId(val value: String)
+
 data class LaundrySymbol(
-    val id: String,
+    val id: SymbolId,
     val name: String,
     val description: String,
     @DrawableRes val drawableId: Int,
