8ce241ee94ee4dc185d6430cf18120eb772cb784
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-25 18:49:10 +0200

    LaundrySymbols adding extension methods in model

--- app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
+++ app/src/main/java/com/tomerpacific/laundry/model/LaundrySymbol.kt
@@ -10,4 +10,21 @@ data class LaundrySymbol(
     val temperature: Int? = null,
     val descriptionFahrenheit: String? = null,
     @DrawableRes val drawableIdFahrenheit: Int? = null
-)
+) {
+    fun descriptionFor(unit: TemperatureUnit): String {
+        return if (unit == TemperatureUnit.FAHRENHEIT) {
+            descriptionFahrenheit ?: description
+        } else {
+            description
+        }
+    }
+
+    @DrawableRes
+    fun drawableIdFor(unit: TemperatureUnit): Int {
+        return if (unit == TemperatureUnit.FAHRENHEIT) {
+            drawableIdFahrenheit ?: drawableId
+        } else {
+            drawableId
+        }
+    }
+}
