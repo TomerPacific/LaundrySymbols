faa6934834199ac9408418ad27521facaf29dc35
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-27 17:36:22 +0300

    LaundrySymbols adding font to typography

--- app/src/main/java/com/tomerpacific/laundry/Type.kt
+++ app/src/main/java/com/tomerpacific/laundry/Type.kt
@@ -1,7 +1,23 @@
 package com.tomerpacific.laundry
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 val Bangers = FontFamily(Font(R.font.bangers_regular, FontWeight.W400))
+
+val LaundryTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Bangers,
+        fontWeight = FontWeight.W400,
+        fontSize = 30.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Bangers,
+        fontWeight = FontWeight.W400,
+        fontSize = 25.sp
+    )
+)
