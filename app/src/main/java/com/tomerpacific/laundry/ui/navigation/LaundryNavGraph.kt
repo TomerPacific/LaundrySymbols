904e618a8d73de270ef641539e7672ae11f20fdc
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-28 15:13:11 +0300

    LaundrySymbols handling event

--- app/src/main/java/com/tomerpacific/laundry/ui/navigation/LaundryNavGraph.kt
+++ app/src/main/java/com/tomerpacific/laundry/ui/navigation/LaundryNavGraph.kt
@@ -1,8 +1,10 @@
 package com.tomerpacific.laundry.ui.navigation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -10,17 +12,31 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.tomerpacific.laundry.LAUNDRY_SYMBOL_KEY
+import com.tomerpacific.laundry.R
 import com.tomerpacific.laundry.ui.screens.HowToDoLaundryScreen
 import com.tomerpacific.laundry.ui.screens.LaundryCategoriesScreen
 import com.tomerpacific.laundry.ui.screens.LaundryCategoryScreen
 import com.tomerpacific.laundry.ui.screens.LaundrySymbolScreen
+import com.tomerpacific.laundry.viewmodel.MainUiEvent
 import com.tomerpacific.laundry.viewmodel.MainViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun LaundryNavGraph(navController: NavHostController, viewModel: MainViewModel) {
     val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
     val validCategoryIds = remember(viewModel.laundryCategories) {
         viewModel.laundryCategories.map { it.labelResId }.toSet()
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.uiEvents.collectLatest { event ->
+            when (event) {
+                is MainUiEvent.ShowNoBrowserError -> {
+                    Toast.makeText(context, context.getString(R.string.error_opening_link), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     NavHost(navController = navController, startDestination = "laundryCategories") {
